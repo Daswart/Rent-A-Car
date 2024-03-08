63533edaca7fefbe7c0dce6eb548c18d8cf72900
Daan Swart <dswart@roc-dev.com>
2024-03-08 18:57:41 +0100

    SECTIE: Onze gegevens.

--- index.php
+++ index.php
@@ -399,6 +399,52 @@
             </div>
         </div>
     </div>
+
+    <!-- SECTIE: Onze Gegevens -->
+    <h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">ONZE GEGEVENS</h2>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-8 p-4 mb-lg-0 mb-3 bg-white rounded">
+                <iframe class="w-100" height="450" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2434.7885849820514!2d5.272490892949089!3d52.39238277355362!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c617c1dcedf6af%3A0xb7e60f149191e0f!2sMBO%20College%20Almere%20-%20ROC%20van%20Flevoland!5e0!3m2!1snl!2snl!4v1709920459204!5m2!1snl!2snl" loading="lazy"></iframe>
+            </div>
+            <div class="col-lg-4 col-md-4">
+                <div class="bg-white p-4 rounded mb-4">
+                    <h5>Neem contact met ons op:</h5>
+                    <a href="TEL +917778889991" class="d-inline-block mb-2 text-decoration-none text-dark">
+                        <i class="bi bi-telephone-fill"></i> (036) 123 45 67
+                    </a>
+                    <br>
+                    <a href="TEL +917778889991" class="d-inline-block mb-2 text-decoration-none text-dark">
+                        <i class="bi bi-envelope-fill"></i> rent-a-car@mail.com
+                    </a>
+                </div>
+                <div class="bg-white p-4 rounded mb-4">
+                    <h5>Adres</h5>
+                    <div class="d-inline-block mb-3">
+                        <span class="badge bg-light text-dark fs-6 p-2">
+                            <i class="bi bi-geo-alt-fill"></i>
+                            Autopad 12
+                        </span>
+                    </div>
+                    <br>
+                    <div class="d-inline-block mb-3">
+                        <span class="badge bg-light text-dark fs-6 p-2">
+                            <i class="bi bi-house-fill"></i>
+                            1335 YY
+                        </span>
+                    </div>
+                    <br>
+                    <div class="d-inline-block mb-3">
+                        <span class="badge bg-light text-dark fs-6 p-2">
+                            <i class="bi bi-buildings-fill"></i>
+                            Almere
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     <br><br><br>
     <br><br><br>
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
