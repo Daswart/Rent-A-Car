6c5143e0614b00afe0e6fe8a6e3a899c4051b8de
Daan Swart <dswart@roc-dev.com>
2024-03-12 13:22:17 +0100

    Contact pagina aangemaakt

--- includes/header.php
+++ includes/header.php
@@ -21,7 +21,7 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active me-2" aria-current="page" href="#">Home</a>
+                        <a class="nav-link active me-2" aria-current="page" href="index.php">Home</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link me-2" href="#">Auto huren</a>
@@ -33,7 +33,7 @@
                         <a class="nav-link me-2" href="#">Contact</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Over Ons</a>
+                        <a class="nav-link" href="over_ons.php">Over Ons</a>
                     </li>
 
                 </ul>
