b0685ff18152b1e138f46bd08d134627df3f5458
Daan Swart <dswart@roc-dev.com>
2024-03-12 11:33:50 +0100

    Aparte files voor header, footer en links.

---
+++ includes/footer.php
@@ -0,0 +1,53 @@
+ <!-- Footer -->
+ <div class="container-fluid bg-white mt-5">
+     <div class="row">
+         <div class="col-lg-4 p-4">
+             <h3 class="h-font fw-bold mb-2">Rent-A-Car</h3>
+             Lorem ipsum dolor sit amet consectetur adipisicing elit.
+             Ullam doloribus aperiam facilis atque. Laboriosam aspernatur
+             corrupti vero, facilis maiores, veniam aperiam nam fuga maxime
+             animi architecto quibusdam, libero quasi incidunt.
+         </div>
+         <div class="col-lg-4 p-4">
+             <h5 class="mb-3">Links</h5>
+             <a class="d-inline-block mb-2 text-dark text-decoration-none" href="">Home</a><br>
+             <a class="d-inline-block mb-2 text-dark text-decoration-none" href="">Auto huren</a><br>
+             <a class="d-inline-block mb-2 text-dark text-decoration-none" href="">Busje Huren</a><br>
+             <a class="d-inline-block mb-2 text-dark text-decoration-none" href="">Contact</a><br>
+             <a class="d-inline-block mb-2 text-dark text-decoration-none" href="">Over Ons</a>
+         </div>
+         <div class="col-lg-4 p-4">
+             <h5 class="mb-3">Volg Ons</h5>
+             <a href="#" class="d-inline-block text-dark text-decoration-none mb-2">
+                 <i class="bi bi-twitter me-1"></i>Twitter
+             </a><br>
+             <a href="#" class="d-inline-block text-dark text-decoration-none mb-2">
+                 <i class="bi bi-facebook me-1"></i>Facebook
+             </a><br>
+             <a href="#" class="d-inline-block text-dark text-decoration-none">
+                 <i class="bi bi-instagram me-1"></i>Instagram
+             </a>
+         </div>
+     </div>
+ </div>
+
+ <h6 class="text-center bg-dark text-white p-3 m-0">Ontworpen & Ontwikkeld door <span class="fst-italic">SherryDesign</span></h6>
+
+ <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+ <!-- Initieer Slider -->
+ <script>
+     var swiper = new Swiper(".swiper-container", {
+         spaceBetween: 30,
+         effect: "fade",
+         loop: true,
+         autoplay: {
+             delay: 3500,
+             disableOnInteraction: false,
+         }
+     });
+ </script>
+ </body>
+
+ </html>
