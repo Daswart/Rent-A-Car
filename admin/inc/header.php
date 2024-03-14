69683875f771a780b4a1ff60fb22b534a2f7e71c
Daan Swart <dswart@roc-dev.com>
2024-03-14 12:59:09 +0100

    Aangemaakt: Dashboard admin panel sidenav

---
+++ admin/inc/header.php
@@ -0,0 +1,34 @@
+ <div class="container-fluid bg-dark text-light p-3 d-flex align-items-center justify-content-between" id="admin-nav">
+     <h3 class="mb-0 h-font ">Rent-A-Car</h3>
+     <a href="uitloggen.php" class="btn btn-light btn-sm">UITLOGGEN</a>
+ </div>
+
+ <div class="col-lg-2 bg-dark border-top border-3 border-secondary" id="dashboard-menu">
+     <nav class="navbar navbar-expand-lg navbar-dark">
+         <div class="container-fluid flex-lg-column align-items-stretch">
+             <h4 class="mt-2 text-light">ADMIN PANEL</h4>
+             <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#adminDropdown" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                 <span class="navbar-toggler-icon"></span>
+             </button>
+             <div class="collapse navbar-collapse flex-column align-items-stretch mt-2" id="adminDropdown">
+                 <ul class="nav nav-pills flex-column">
+                     <li class="nav-item">
+                         <a class="nav-link text-white" href="dashboard.php">Dashboard</a>
+                     </li>
+                     <li class="nav-item">
+                         <a class="nav-link text-white" href="#">Auto's</a>
+                     </li>
+                     <li class="nav-item">
+                         <a class="nav-link text-white" href="#">Busjes</a>
+                     </li>
+                     <li class="nav-item">
+                         <a class="nav-link text-white" href="#">Gebruikers</a>
+                     </li>
+                     <li class="nav-item">
+                         <a class="nav-link text-white" href="#">Instellingen</a>
+                     </li>
+                 </ul>
+             </div>
+         </div>
+     </nav>
+ </div>
