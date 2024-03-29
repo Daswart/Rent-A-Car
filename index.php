a33cfeeb0b8c1cba02f87d0823f32b547db7c2d9
Daan Swart <dswart@roc-dev.com>
2024-03-29 13:44:45 +0100

    Ophalen afbeeldigen carousel voor in de header swiper

--- index.php
+++ index.php
@@ -32,15 +32,19 @@
     <div class="container-fluid px-lg-4 mt-4">
         <div class="swiper swiper-container">
             <div class="swiper-wrapper">
-                <div class="swiper-slide">
-                    <img src="images/carousel/1.png" class="w-100 d-block" />
-                </div>
-                <div class="swiper-slide">
-                    <img src="images/carousel/2.png" class="w-100 d-block" />
-                </div>
-                <div class="swiper-slide">
-                    <img src="images/carousel/3.png" class="w-100 d-block" />
-                </div>
+                <?php
+                $res = selectAll('carousel');
+
+                while ($row = mysqli_fetch_assoc($res)) {
+                    $path = CAROUSEL_IMG_PATH;
+                    echo <<<data
+                        
+                        <div class="swiper-slide">
+                         <img src="$path$row[image]" class="w-100 d-block" />
+                        </div>
+                    data;
+                }
+                ?>
             </div>
             <div class="swiper-pagination"></div>
         </div>
