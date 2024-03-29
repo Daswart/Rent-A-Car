d8b27167eb9f829eca06b710b7c80b9c213891c3
Daan Swart <dswart@roc-dev.com>
2024-03-29 13:35:15 +0100

    Verwijderen carousel afbeelding functie

--- admin/ajax/carousel_crud.php
+++ admin/ajax/carousel_crud.php
@@ -34,7 +34,7 @@ if (isset($_POST['get_carousel'])) {
             <div class="card bg-dark text-white">
                 <img src="$path$row[image]" class="card-img" alt="...">
                 <div class="card-img-overlay text-end">
-                    <button type="button" onclick="rem_member($row[sr_no])" class="btn btn-danger btn-sm shadow-none">
+                    <button type="button" onclick="rem_image($row[sr_no])" class="btn btn-danger btn-sm shadow-none">
                     <i class="bi bi-trash"></i> Delete
                     </button>
                 </div>
@@ -44,16 +44,16 @@ if (isset($_POST['get_carousel'])) {
     }
 }
 
-if (isset($_POST['rem_member'])) {
+if (isset($_POST['rem_image'])) {
     $frm_data = filteration($_POST);
-    $values = [$frm_data['rem_member']];
+    $values = [$frm_data['rem_image']];
 
-    $pre_q = "SELECT * FROM `team_details` WHERE `sr_no`=?";
+    $pre_q = "SELECT * FROM `carousel` WHERE `sr_no`=?";
     $res = select($pre_q, $values, 'i');
     $img = mysqli_fetch_assoc($res);
 
-    if (deleteImage($img['picture'], ABOUT_FOLDER)) {
-        $q = "DELETE FROM `team_details` WHERE `sr_no`=?";
+    if (deleteImage($img['image'], CAROUSEL_FOLDER)) {
+        $q = "DELETE FROM `carousel` WHERE `sr_no`=?";
         $res = deleteRow($q, $values, 'i');
         echo $res;
     } else {
